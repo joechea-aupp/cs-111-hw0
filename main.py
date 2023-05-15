d0aa6b9051df086922c5f3b3a05985ab6d6840c5
Joe Chea <s.chea@aupp.edu.kh>
2023-05-15 22:38:08 +0700

    initial commit for homework 0

---
+++ main.py
@@ -0,0 +1,20 @@
+input("Welcome to Dog Age to Human Age Conversion APP")
+print("Please provide the following information")
+print("----------------------------------------")
+print("\n")
+# create variable to capture input from your user
+# task 1: ask for your user for name
+# Your name:
+# task 2: ask for your user's dog age
+# Your dog age:
+# now calculating your user dog age to human age {dog_age * 7}
+# important: make sure above calculation return result as integer.
+
+# now display your user input and their dog information on screen following the below format
+"""
+Thank you for using our APP {user's name}
+
+--------------------------------------------------------
+Your dog age equivalent to human is {dog age converted to human} years old
+--------------------------------------------------------
+"""
